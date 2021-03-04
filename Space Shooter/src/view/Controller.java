412a5200c202c37d539ab662f653f8bb21ecc209
Bharath Routhu <brr945@cs.bham.ac.uk>
2021-03-04 13:37:20 +0000

    added the shipView to empty Maze

---
+++ Space Shooter/src/view/Controller.java
@@ -0,0 +1,27 @@
+package view;
+
+import javafx.fxml.FXML;
+
+import javafx.event.EventHandler;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.application.Platform;
+
+public class Controller implements EventHandler<KeyEvent> {
+
+    @FXML
+    private GameView gameView;
+    private ShipModel shipModel;
+    private static final String[] levelFiles = {"Space Shooter/src/levels/level1.txt"};
+    final private static double FRAMES_PER_SECOND = 5.0;
+    private Timer timer;
+
+    public Controller() {}
+
+    public void initialize() {
+    	//String file = this.getLevelFile(0);
+        
+        this.shipModel = new ShipModel();
+        this.update(ShipModel.Direction.NONE);
+        this.startTimer();
+    }
