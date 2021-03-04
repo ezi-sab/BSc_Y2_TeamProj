99f6365fa1d468a9ced8abc6e19ffdab40cf8e97
Bharath Routhu <brr945@cs.bham.ac.uk>
2021-03-04 15:32:42 +0000

    added necessary imports and timer method

--- Space Shooter/src/view/Controller.java
+++ Space Shooter/src/view/Controller.java
@@ -5,6 +5,12 @@ import javafx.fxml.FXML;
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
 
+import javafx.scene.input.KeyEvent;
+import javafx.application.Platform;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Controller implements EventHandler<KeyEvent> {
 
     @FXML
@@ -23,3 +29,19 @@ public class Controller implements EventHandler<KeyEvent> {
         this.update(ShipModel.Direction.NONE);
         this.startTimer();
     }
+
+
+    private void startTimer() {
+        this.timer = new java.util.Timer();
+        TimerTask timerTask = new TimerTask() {
+            public void run() {
+                Platform.runLater(() -> update(ShipModel.getCurrentDirection()));
+            }
+        };
+
+        long frameTimeInMilliseconds = (long) (1000.0 / FRAMES_PER_SECOND);
+        this.timer.schedule(timerTask, 0, frameTimeInMilliseconds);
+    }
+
+
+    
