8ee4c92b9b87c94eecc299e4b7a8eb7877eb3271
Bharath Routhu <brr945@cs.bham.ac.uk>
2021-03-04 13:39:12 +0000

    Removed unused imports.

--- Space Shooter/src/view/Controller.java
+++ Space Shooter/src/view/Controller.java
@@ -4,8 +4,6 @@ import javafx.fxml.FXML;
 
 import javafx.event.EventHandler;
 import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.application.Platform;
 
 public class Controller implements EventHandler<KeyEvent> {
 
