f5303f151005b8ad7296f48e04cc1fd739dc9083
Bharath Routhu <brr945@cs.bham.ac.uk>
2021-03-15 12:19:27 +0000

    removed duplicates and added settings sub scene

--- Space Shooter/src/application/Main.java
+++ Space Shooter/src/application/Main.java
@@ -11,7 +11,7 @@ import view.ViewManager;
 
 public class Main extends Application {
 
-	ViewManager viewManager = new ViewManager();
+	// ViewManager viewManager = new ViewManager();
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -20,7 +20,7 @@ public class Main extends Application {
 			//Scene scene = new Scene(root,400,400);
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			//primaryStage.setScene(scene);
-			viewManager.playSound("Space Shooter/src/view/resources/sounds/spaceinvaders1.mp3");
+			// viewManager.playSound("Space Shooter/src/view/resources/sounds/spaceinvaders1.mp3");
 			ViewManager manager = new ViewManager();
 			primaryStage = manager.getMainStage();
 			primaryStage.show();
