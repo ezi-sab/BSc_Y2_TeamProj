334da0a8f337e0f5bc957bd56e73db6bd81b9f59
exk986 <exk986@cs.bham.ac.uk>
2021-03-18 00:23:00 +0900

    Updated Main.java

--- Space Shooter/src/application/Main.java
+++ Space Shooter/src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import javafx.application.Application;
 
 import javafx.stage.Stage;
@@ -10,26 +10,23 @@ import view.ViewManager;
 
 
 public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            //BorderPane root = new BorderPane();
+            //Scene scene = new Scene(root,400,400);
+            //scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            //primaryStage.setScene(scene);
+            ViewManager manager = new ViewManager();
+            manager.playSound("src/view/resources/sounds/spaceinvaders1.mp3");
+            primaryStage = manager.getMainStage();
+            primaryStage.show();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	ViewManager viewManager = new ViewManager();
-
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			//BorderPane root = new BorderPane();
-			//Scene scene = new Scene(root,400,400);
-			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//primaryStage.setScene(scene);
-			viewManager.playSound("Space Shooter/src/view/resources/sounds/spaceinvaders1.mp3");
-			ViewManager manager = new ViewManager();
-			primaryStage = manager.getMainStage();
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
