6a0fc3d8a4654fbf5dec0cdf42979603fb43ded9
exk986 <exk986@cs.bham.ac.uk>
2021-03-18 05:48:29 +0900

    Updated src/view

--- Space Shooter/src/view/ShipModel.java
+++ Space Shooter/src/view/ShipModel.java
@@ -2,16 +2,20 @@ package view;
 
 import javafx.geometry.Point2D;
 
+
+
 import javafx.fxml.FXML;
 
 import java.io.*;
 
 import java.util.*;
 
+
+
 public class ShipModel {
 
     public enum CellValue {
-        EMPTY, SHIPSTARTINGPOINT
+        EMPTY, SHIPSTARTINGPOINT, BLOCK, FLAG, ENEMY1STARTINGPOINT, ENEMY2STARTINGPOINT, COIN
     }
 
     public enum Direction {
@@ -24,9 +28,20 @@ public class ShipModel {
     private int columnCount;
     private CellValue[][] grid;
     private Point2D shipLocation;
+    private Point2D enemy1Location;
+    private Point2D enemy2Location;
     private Point2D shipVelocity;
+    private Point2D enemy1Velocity;
+    private Point2D enemy2Velocity;
     private static Direction lastDirection;
     private static Direction currentDirection;
+
+
+    private int score;
+    private int level;
+    private int flagCount;
+    private static boolean gameOver;
+    private static boolean youWon;
 
     public ShipModel() {
         this.startNewGame();
@@ -43,7 +58,7 @@ public class ShipModel {
         while (scanner.hasNextLine()) {
             String line = scanner.nextLine();
             @SuppressWarnings("resource")
-			Scanner lineScanner = new Scanner(line);
+            Scanner lineScanner = new Scanner(line);
             while (lineScanner.hasNext()) {
                 lineScanner.next();
                 columnCount++;
@@ -58,16 +73,20 @@ public class ShipModel {
             e.printStackTrace();
         }
         grid = new CellValue[rowCount][columnCount];
-        
+
         int row = 0;
-        int pacmanRow = 0;
-        int pacmanColumn = 0;
-        
+        int shipRow = 0;
+        int shipColumn = 0;
+        int enemy1Row = 0;
+        int enemy1Column = 0;
+        int enemy2Row = 0;
+        int enemy2Column = 0;
+
         while (scanner2.hasNextLine()) {
             int column = 0;
             String line = scanner2.nextLine();
             @SuppressWarnings("resource")
-			Scanner lineScanner = new Scanner(line);
+            Scanner lineScanner = new Scanner(line);
             while (lineScanner.hasNext()) {
                 String value = lineScanner.next();
                 CellValue thisValue;
@@ -75,46 +94,190 @@ public class ShipModel {
                     case "P":
                     {
                         thisValue = CellValue.SHIPSTARTINGPOINT;
-                        pacmanRow = row;
-                        pacmanColumn = column;
+                        shipRow = row;
+                        shipColumn = column;
                         break;
                     }
+                    case "1":
+                    {
+                        thisValue = CellValue.ENEMY1STARTINGPOINT;
+                        enemy1Row = row;
+                        enemy1Column = column;
+                    }
+                    case "2":
+                    {
+                        thisValue = CellValue.ENEMY2STARTINGPOINT;
+                        enemy2Row = row;
+                        enemy2Column = column;
+                    }
+                    case "B":
+                        thisValue = CellValue.BLOCK;
+                        break;
+                    case "F":
+                        thisValue = CellValue.FLAG;
+                        break;
                     default:
-                    	thisValue = CellValue.EMPTY;
+                        thisValue = CellValue.EMPTY;
                 }
                 grid[row][column] = thisValue;
                 column++;
             }
             row++;
         }
-        shipLocation = new Point2D(pacmanRow, pacmanColumn);
+        shipLocation = new Point2D(shipRow, shipColumn);
         shipVelocity = new Point2D(0, 0);
         currentDirection = Direction.NONE;
         lastDirection = Direction.NONE;
+        enemy1Location = new Point2D(enemy1Row,enemy1Column);
+        enemy1Velocity = new Point2D(-1, 0);
+        enemy2Location = new Point2D(enemy2Row,enemy2Column);
+        enemy2Velocity = new Point2D(-1, 0);
     }
 
     public void startNewGame(){
         rowCount = 0;
         columnCount = 0;
-        this.initializeLevel(Controller.getLevelFile(0));
+        this.gameOver = false;
+        this.youWon = false;
+        flagCount = 0;
+        this.score = 0;
+        this.level = 1;
+        this.initializeLevel(Controller.getCurrentLevel(0));
     }
 
     public void moveShip(Direction direction) {
         Point2D predictedShipVelocity = changeVelocity(direction);
         Point2D predictedShipLocation = shipLocation.add(predictedShipVelocity);
+        predictedShipLocation = setOffScreenLocation(predictedShipLocation);
         if (direction.equals(lastDirection)) {
-        	shipVelocity = predictedShipVelocity;
-            shipLocation = predictedShipLocation;
-        } else {
-        	shipVelocity = predictedShipVelocity;
-            shipLocation = predictedShipLocation;
-            setLastDirection(direction);
+            if (grid[(int) predictedShipLocation.getX()][(int) predictedShipLocation.getY()] == CellValue.BLOCK) {
+                shipVelocity = changeVelocity(Direction.NONE);
+            } else {
+                shipVelocity = predictedShipVelocity;
+                shipLocation = predictedShipLocation;
+                setLastDirection(direction);
             }
         }
+        else {
+            if (grid[(int) predictedShipLocation.getX()][(int) predictedShipLocation.getY()] == CellValue.BLOCK) {
+                predictedShipVelocity = changeVelocity(lastDirection);
+                predictedShipLocation = shipLocation.add(predictedShipVelocity);
+                if (grid[(int) predictedShipLocation.getX()][(int) predictedShipLocation.getY()] == CellValue.BLOCK) {
+                    shipVelocity = changeVelocity(Direction.NONE);
+                } else {
+                    shipVelocity = changeVelocity(lastDirection);
+                    shipLocation = shipLocation.add(shipVelocity);
+                }
+            } else {
+                shipVelocity = predictedShipVelocity;
+                shipLocation = predictedShipLocation;
+                setLastDirection(direction);
+            }
+        }
+    }
+
+    public Point2D[] moveEnemy(Point2D velocity, Point2D location){
+        Random generator = new Random();
+
+        if (location.getY() == shipLocation.getY()) {
+            if (location.getX() > shipLocation.getX()) {
+                velocity = changeVelocity(Direction.UP);
+            } else {
+                velocity = changeVelocity(Direction.DOWN);
+            }
+            Point2D predictedLocation = location.add(velocity);
+            //if the ghost would go offscreen, wrap around
+            predictedLocation = setOffScreenLocation(predictedLocation);
+            //generate new random directions until ghost can move without hitting a wall
+            while (grid[(int) predictedLocation.getX()][(int) predictedLocation.getY()] == CellValue.BLOCK) {
+                Direction direction = randomDirectionGenerator();
+                velocity = changeVelocity(direction);
+                predictedLocation = location.add(velocity);
+            }
+            location = predictedLocation;
+        }
+        //check if ghost is in PacMan's row and move towards him
+        else if (location.getX() == shipLocation.getX()) {
+            if (location.getY() > shipLocation.getY()) {
+                velocity = changeVelocity(Direction.LEFT);
+            } else {
+                velocity = changeVelocity(Direction.RIGHT);
+            }
+            Point2D potentialLocation = location.add(velocity);
+            potentialLocation = setOffScreenLocation(potentialLocation);
+            while (grid[(int) potentialLocation.getX()][(int) potentialLocation.getY()] == CellValue.BLOCK) {
+                Direction direction = randomDirectionGenerator();
+                velocity = changeVelocity(direction);
+                potentialLocation = location.add(velocity);
+            }
+            location = potentialLocation;
+        }
+        //move in a consistent random direction until it hits a wall, then choose a new random direction
+        else{
+            Point2D potentialLocation = location.add(velocity);
+            potentialLocation = setOffScreenLocation(potentialLocation);
+            while(grid[(int) potentialLocation.getX()][(int) potentialLocation.getY()] == CellValue.BLOCK){
+                Direction direction = randomDirectionGenerator();
+                velocity = changeVelocity(direction);
+                potentialLocation = location.add(velocity);
+            }
+            location = potentialLocation;
+        }
+
+        Point2D[] data = {velocity, location};
+        return data;
+
+    }
+
+    public Point2D setOffScreenLocation(Point2D objectLocation) {
+        if (objectLocation.getY() >= columnCount) {
+            objectLocation = new Point2D(objectLocation.getX(), 0);
+        }
+        if (objectLocation.getY() < 0) {
+            objectLocation = new Point2D(objectLocation.getX(), columnCount - 1);
+        }
+
+        if (objectLocation.getX() >= rowCount) {
+            objectLocation = new Point2D(0, objectLocation.getY());
+        }
+        if (objectLocation.getX() < 0) {
+            objectLocation = new Point2D(rowCount -1, objectLocation.getY());
+        }
+        return objectLocation;
+    }
+
+    public Direction randomDirectionGenerator(){
+        Random generator = new Random();
+        int randInt = generator.nextInt(4);
+        if (randInt == 0){
+            return Direction.LEFT;
+        }
+        else if (randInt == 1){
+            return Direction.RIGHT;
+        }
+        else if(randInt == 2){
+            return Direction.UP;
+        }
+        else{
+            return Direction.DOWN;
+        }
+    }
 
 
     public void step(Direction direction) {
         this.moveShip(direction);
+        CellValue shipLocationCellValue = grid[(int) shipLocation.getX()][(int) shipLocation.getY()];
+        if (shipLocationCellValue == CellValue.COIN) {
+            grid[(int) shipLocation.getX()][(int) shipLocation.getY()] = CellValue.EMPTY;
+        }
+        if (shipLocation.equals(enemy1Location)) {
+            gameOver = true;
+            shipVelocity = new Point2D(0,0);
+        }
+        if (shipLocation.equals(enemy2Location)) {
+            gameOver = true;
+            shipVelocity = new Point2D(0,0);
+        }
     }
 
     public Point2D changeVelocity(Direction direction) {
@@ -162,5 +325,61 @@ public class ShipModel {
 
     public Point2D getShipLocation() {
         return shipLocation;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public static boolean isGameOver() {
+        return gameOver;
+    }
+
+    public static boolean isYouWon() {
+        return youWon;
+    }
+
+    public Point2D getEnemy1Location() {
+        return enemy1Location;
+    }
+
+    public void setEnemy1Location(Point2D enemy1Location) {
+        this.enemy1Location = enemy1Location;
+    }
+
+    public Point2D getEnemy2Location() {
+        return enemy2Location;
+    }
+
+    public void setEnemy2Location(Point2D enemy2Location) {
+        this.enemy2Location = enemy2Location;
+    }
+
+    public Point2D getEnemy1Velocity() {
+        return enemy1Velocity;
+    }
+
+    public void setEnemy1Velocity(Point2D enemy1Velocity) {
+        this.enemy1Velocity = enemy1Velocity;
+    }
+
+    public Point2D getEnemy2Velocity() {
+        return enemy2Velocity;
+    }
+
+    public void setEnemy2Velocity(Point2D enemy2Velocity) {
+        this.enemy2Velocity = enemy2Velocity;
     }
 }
--- Space Shooter/src/view/ViewManager.java
+++ Space Shooter/src/view/ViewManager.java
@@ -1,8 +1,8 @@
 package view;
 
-
 import java.nio.file.Paths;
 import java.util.ArrayList;
+
 import java.util.List;
 
 import javafx.event.ActionEvent;
@@ -28,300 +28,279 @@ import model.buttons;
 import model.menuSubScene;
 
 public class ViewManager {
-	
-	private static final int width = 1024;
-	private static final int height = 768;
-	private AnchorPane mainPane;
-	private Scene mainScene;
-	private Stage mainStage;
-	
-	private final static int MENU_BUTTON_STARTX = 100;
-	private final static int MENU_BUTTON_STARTY = 220;
-	
-	private menuSubScene shipSelectSubScene;
-	private menuSubScene settingsSubScene;
-	private menuSubScene helpSubScene;
-	private menuSubScene creditsSubScene;
-	
-	private menuSubScene sceneToHide;
 
-	List<buttons> menuButtons;
-	
-	List<ShipPicker> shipsList;
-	private Ship chosenShip;
-	
-	public ViewManager() {
-		menuButtons = new ArrayList<>();
-		mainPane = new AnchorPane();
-		mainScene = new Scene(mainPane, width, height);
-		mainStage = new Stage();
-		mainStage.setScene(mainScene);
-		createSubScene();
-		createButtons();
-		createBackground();
-		createLogo();
-		
+    private static final int width = 1024;
+    private static final int height = 768;
+    private AnchorPane mainPane;
+    private Scene mainScene;
+    private Stage mainStage;
 
-	}
+    private final static int MENU_BUTTON_STARTX = 100;
+    private final static int MENU_BUTTON_STARTY = 220;
 
-	private void createSubScene() {
-		
-		settingsSubScene = new menuSubScene();
-		mainPane.getChildren().add(settingsSubScene);
-		
-		helpSubScene = new menuSubScene();
-		mainPane.getChildren().add(helpSubScene);
-		
-		creditsSubScene = new menuSubScene();
-		mainPane.getChildren().add(creditsSubScene);
-		
-		createShipSelectSubScene();
-		
-		
-	}
+    private menuSubScene shipSelectSubScene;
+    private menuSubScene settingsSubScene;
+    private menuSubScene helpSubScene;
+    private menuSubScene creditsSubScene;
+
+    private menuSubScene sceneToHide;
+
+    List<buttons> menuButtons;
+
+    List<ShipPicker> shipsList;
+    private Ship chosenShip;
+
+    public ViewManager() {
+        menuButtons = new ArrayList<>();
+        mainPane = new AnchorPane();
+        mainScene = new Scene(mainPane, width, height);
+        mainStage = new Stage();
+        mainStage.setScene(mainScene);
+        createSubScene();
+        createButtons();
+        createBackground();
+        createLogo();
 
 
-	private void showSubScene(menuSubScene subScene) {
-		if(sceneToHide != null) {
-			sceneToHide.moveSubScene();
-		}
-		
-		subScene.moveSubScene();
-		sceneToHide= subScene;
-	}
+    }
+
+    private void showSubScene(menuSubScene subScene) {
+        if(sceneToHide != null) {
+            sceneToHide.moveSubScene();
+        }
+
+        subScene.moveSubScene();
+        sceneToHide= subScene;
+    }
+
+    private void createSubScene() {
+
+        //shipSelectSubScene = new menuSubScene();
+        //mainPane.getChildren().add(shipSelectSubScene);
+
+        settingsSubScene = new menuSubScene();
+        mainPane.getChildren().add(settingsSubScene);
+
+        helpSubScene = new menuSubScene();
+        mainPane.getChildren().add(helpSubScene);
+
+        creditsSubScene = new menuSubScene();
+        mainPane.getChildren().add(creditsSubScene);
+
+        createShipSelectSubScene();
 
 
+    }
 
-	private void createShipSelectSubScene() {
-		shipSelectSubScene = new menuSubScene();
-		mainPane.getChildren().add(shipSelectSubScene);
-		
-		InfoLabel chooseShipLabel = new InfoLabel("CHOOSE YOUR SHIP");
-		chooseShipLabel.setLayoutX(110);
-		chooseShipLabel.setLayoutY(25);
-		shipSelectSubScene.getPane().getChildren().add(chooseShipLabel);
-		shipSelectSubScene.getPane().getChildren().add(createShipsToChoose());
-		shipSelectSubScene.getPane().getChildren().add(createButtonToStart());
-		
-	}
+    private void createShipSelectSubScene() {
+        shipSelectSubScene = new menuSubScene();
+        mainPane.getChildren().add(shipSelectSubScene);
 
+        InfoLabel chooseShipLabel = new InfoLabel("CHOOSE YOUR SHIP");
+        chooseShipLabel.setLayoutX(110);
+        chooseShipLabel.setLayoutY(25);
+        shipSelectSubScene.getPane().getChildren().add(chooseShipLabel);
+        shipSelectSubScene.getPane().getChildren().add(createShipsToChoose());
+        shipSelectSubScene.getPane().getChildren().add(createButtonToStart());
 
+    }
 
-	private buttons createButtonToStart() {
-		buttons startButton = new buttons("START");
-		startButton.setLayoutX(200);
-		startButton.setLayoutY(300);
-		
-		startButton.setOnAction(new EventHandler<ActionEvent>() {
+    private HBox createShipsToChoose() {
+        HBox box = new HBox();
+        box.setSpacing(20);
+        shipsList = new ArrayList<>();
+        for (Ship ship : Ship.values()) {
+            ShipPicker shipToPick = new ShipPicker(ship);
+            shipsList.add(shipToPick);
+            box.getChildren().add(shipToPick);
+            shipToPick.setOnMouseClicked(new EventHandler<MouseEvent>() {
 
-			@Override
-			public void handle(ActionEvent event) {
-				if (chosenShip != null) {
-					GameView gameManager = new GameView();
-					try {
-						gameManager.createNewGame(mainStage, chosenShip);
-					} catch (Exception e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-				
-			}
-			
-		});
-	
-		return startButton;
-	}
+                @Override
+                public void handle(MouseEvent event) {
+                    for (ShipPicker ship:shipsList) {
+                        ship.setIsCircleChosen(false);
+                    }
+                    shipToPick.setIsCircleChosen(true);
+                    chosenShip = shipToPick.getShip();
+                }
 
+            });
+        }
+        box.setLayoutX(300-(118*2));
+        box.setLayoutY(100);
+        return box;
 
+    }
 
-	private HBox createShipsToChoose() {
-		HBox box = new HBox();
-		box.setSpacing(20);
-		shipsList = new ArrayList<>();
-		for (Ship ship : Ship.values()) {
-			ShipPicker shipToPick = new ShipPicker(ship);
-			shipsList.add(shipToPick);
-			box.getChildren().add(shipToPick);
-			shipToPick.setOnMouseClicked(new EventHandler<MouseEvent>() {
+    private buttons createButtonToStart() {
+        buttons startButton = new buttons("START");
+        startButton.setLayoutX(200);
+        startButton.setLayoutY(300);
 
-				@Override
-				public void handle(MouseEvent event) {
-					for (ShipPicker ship:shipsList) {
-						ship.setIsCircleChosen(false);
-					}
-					shipToPick.setIsCircleChosen(true);
-					chosenShip = shipToPick.getShip();
-				}
-				
-			});
-		}
-		box.setLayoutX(300-(118*2));
-		box.setLayoutY(100);
-		return box;
-		
-	}
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
 
-	public Stage getMainStage() {
-		return mainStage;
-	}
-	
-	private void addMenuButton(buttons button) {
-		button.setLayoutX(MENU_BUTTON_STARTX);
-		button.setLayoutY(MENU_BUTTON_STARTY + menuButtons.size() * 100);
-		menuButtons.add(button);
-		mainPane.getChildren().add(button);
-	}
+            @Override
+            public void handle(ActionEvent event) {
+                if (chosenShip != null) {
+                    GameView gameManager = new GameView();
+                    gameManager.setShipImage(chosenShip);
+                    try {
+                        gameManager.createNewGame(mainStage, chosenShip);
+                    } catch (Exception e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
 
-	private void createButtons() {
-		createStartButton();
-		createHelpButton();
-		createSettingsButton();
-		createCreditsButton();
-		createExitButton();
-	}
+            }
 
-	MediaPlayer mediaPlayer;
+        });
 
-	public void playSound(String soundPath) {
-		String src = soundPath;
-		Media tapped = new Media(Paths.get(src).toUri().toString());
-		mediaPlayer = new MediaPlayer(tapped);
-		mediaPlayer.setCycleCount(1);
-		mediaPlayer.play();
+        return startButton;
+    }
 
-	//		AudioClip tapped = new AudioClip(this.getClass().getResource("view/resources/fastinvader1.wav").toString());
-	//		tapped.play();
+    public Stage getMainStage() {
+        return mainStage;
+    }
 
-	}
+    private void addMenuButton(buttons button) {
+        button.setLayoutX(MENU_BUTTON_STARTX);
+        button.setLayoutY(MENU_BUTTON_STARTY + menuButtons.size() * 100);
+        menuButtons.add(button);
+        mainPane.getChildren().add(button);
+    }
 
+    private void createButtons() {
+        createStartButton();
+        createHelpButton();
+        createSettingsButton();
+        createCreditsButton();
+        createExitButton();
+    }
 
+    private void createStartButton() {
+        buttons startButton = new buttons("PLAY");
+        addMenuButton(startButton);
 
-	private void createStartButton() {
-		buttons startButton = new buttons("PLAY");
-		addMenuButton(startButton);
-		
-		startButton.setOnAction(new EventHandler<ActionEvent>() {
+        startButton.setOnAction(new EventHandler<ActionEvent>() {
 
-			@Override
-			public void handle(ActionEvent event) {
-				playSound("Space Shooter/src/view/resources/sounds/fastinvader1.mp3");
-				showSubScene(shipSelectSubScene);
-			}
-			
-		});
-	}
+            @Override
+            public void handle(ActionEvent event) {
+                playSound("src/view/resources/sounds/fastinvader1.mp3");
+                showSubScene(shipSelectSubScene);
+            }
+
+        });
+    }
+
+    private void createSettingsButton() {
+        buttons settingsButton = new buttons("SETTINGS");
+        addMenuButton(settingsButton);
+
+        settingsButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                playSound("src/view/resources/sounds/fastinvader1.mp3");
+                showSubScene(settingsSubScene);
+            }
 
 
-	private void createHelpButton() {
-		buttons helpButton = new buttons("HELP");
-		addMenuButton(helpButton);
-		
-		helpButton.setOnAction(new EventHandler<ActionEvent>() {
+        });
+    }
 
-			@Override
-			public void handle(ActionEvent event) {
-				playSound("Space Shooter/src/view/resources/sounds/fastinvader1.mp3");
-				showSubScene(helpSubScene);
-			}
-			
-			
-		});
-	}
+    private void createHelpButton() {
+        buttons helpButton = new buttons("HELP");
+        addMenuButton(helpButton);
 
-	private void createSettingsButton() {
-		buttons settingsButton = new buttons("SETTINGS");
-		addMenuButton(settingsButton);
-		
-		settingsButton.setOnAction(new EventHandler<ActionEvent>() {
+        helpButton.setOnAction(new EventHandler<ActionEvent>() {
 
-			@Override
-			public void handle(ActionEvent event) {
-				playSound("Space Shooter/src/view/resources/sounds/fastinvader1.mp3");
-				showSubScene(settingsSubScene);
-			}
-			
-			
-		});
-	}
-
-	private void createCreditsButton() {
-		buttons creditsButton = new buttons("CREDITS");
-		addMenuButton(creditsButton);
-		
-		creditsButton.setOnAction(new EventHandler<ActionEvent>() {
-
-			@Override
-			public void handle(ActionEvent event) {
-				playSound("Space Shooter/src/view/resources/sounds/fastinvader1.mp3");
-				showSubScene(creditsSubScene);
-			}
-			
-			
-		});
-	}
+            @Override
+            public void handle(ActionEvent event) {
+                playSound("src/view/resources/sounds/fastinvader1.mp3");
+                showSubScene(helpSubScene);
+            }
 
 
-	private void createExitButton() {
-		buttons exitButton = new buttons("EXIT");
-		addMenuButton(exitButton);
-		
-		exitButton.setOnAction(new EventHandler<ActionEvent>() {
+        });
+    }
 
-			@Override
-			public void handle(ActionEvent arg0) {
-				playSound("Space Shooter/src/view/resources/sounds/fastinvader1.mp3");
-				mainStage.close();				
-			}
-			
-		});
-	}
+    private void createCreditsButton() {
+        buttons creditsButton = new buttons("CREDITS");
+        addMenuButton(creditsButton);
 
-	private void createBackground() {
-		Image backgroundImage = new Image("view/resources/space.png", 256, 256, false, true);
-		BackgroundImage background = new BackgroundImage(backgroundImage,BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
-		mainPane.setBackground(new Background(background));
-	}
+        creditsButton.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                playSound("src/view/resources/sounds/fastinvader1.mp3");
+                showSubScene(creditsSubScene);
+            }
 
 
-	private void createLogo() {
-		ImageView logo = new ImageView("view/resources/StarShooter.png");
-		logo.setLayoutX(138);
-		logo.setLayoutY(50);
-		
-		logo.setOnMouseEntered(new EventHandler<MouseEvent>() {
+        });
+    }
 
-			@Override
-			public void handle(MouseEvent event) {
-				logo.setEffect(new DropShadow());
-			}
-		});
-		
-		logo.setOnMouseExited(new EventHandler<MouseEvent>() {
+    private void createExitButton() {
+        buttons exitButton = new buttons("EXIT");
+        addMenuButton(exitButton);
 
-			@Override
-			public void handle(MouseEvent event) {
-				logo.setEffect(null);
-			}
-				
-		});
-		
-		mainPane.getChildren().add(logo);
+        exitButton.setOnAction(new EventHandler<ActionEvent>() {
 
-	}
+            @Override
+            public void handle(ActionEvent arg0) {
+                playSound("src/view/resources/sounds/fastinvader1.mp3");
+                mainStage.close();
+            }
+
+        });
+    }
+
+    private void createBackground() {
+        Image backgroundImage = new Image("view/resources/space.png", 256, 256, false, true);
+        BackgroundImage background = new BackgroundImage(backgroundImage,BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.DEFAULT, null);
+        mainPane.setBackground(new Background(background));
+    }
+
+    private void createLogo() {
+        ImageView logo = new ImageView("view/resources/StarShooter.png");
+        logo.setLayoutX(138);
+        logo.setLayoutY(50);
+
+        logo.setOnMouseEntered(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                logo.setEffect(new DropShadow());
+            }
+        });
+
+        logo.setOnMouseExited(new EventHandler<MouseEvent>() {
+
+            @Override
+            public void handle(MouseEvent event) {
+                logo.setEffect(null);
+            }
+
+        });
+
+        mainPane.getChildren().add(logo);
+    }
+
+    MediaPlayer mediaPlayer;
+
+    public void playSound(String soundPath) {
+        String src = soundPath;
+        Media tapped = new Media(Paths.get(src).toUri().toString());
+        mediaPlayer = new MediaPlayer(tapped);
+        mediaPlayer.setCycleCount(1);
+        mediaPlayer.play();
+
+        //		AudioClip tapped = new AudioClip(this.getClass().getResource("view/resources/fastinvader1.wav").toString());
+        //		tapped.play();
+
+    }
 
 
 
 }
-	
-
-
-
-
-
-	
-
-	
-
-
-
