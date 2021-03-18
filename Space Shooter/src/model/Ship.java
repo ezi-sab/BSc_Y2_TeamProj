e359e5690e75587bbc5a3b0ad91208c95f7635c0
rxs1028 <rxs1028@cs.bham.ac.uk>
2021-03-18 18:50:29 +0000

    update

--- Space Shooter/src/model/Ship.java
+++ Space Shooter/src/model/Ship.java
@@ -2,19 +2,48 @@ package model;
 
 public enum Ship{
 	
-	blue("view/resources/shipPicker/playerShip3_blue.png"),
+	/*blue("view/resources/shipPicker/playerShip3_blue.png"),
 	green("view/resources/shipPicker/playerShip3_green.png"),
 	orange("view/resources/shipPicker/playerShip3_orange.png"),
-	red("view/resources/shipPicker/playerShip3_red.png");
+	red("view/resources/shipPicker/playerShip3_red.png"); */
 	
-	private String urlShip;
+	blue("/res/playerShip3_blue.png", "/res/playerShip3_blueD.png", "/res/playerShip3_blueL.png", "/res/playerShip3_blueR.png", "/res/playerLife3_blue.png"),
+	green("/res/playerShip3_green.png", "/res/playerShip3_greenD.png", "/res/playerShip3_greenL.png", "/res/playerShip3_greenR.png", "/res/playerLife3_green.png"),
+	orange("/res/playerShip3_orange.png", "/res/playerShip3_orangeD.png", "/res/playerShip3_orangeL.png", "/res/playerShip3_orangeR.png", "/res/playerLife3_orange.png"),
+	red("/res/playerShip3_red.png", "/res/playerShip3_redD.png", "/res/playerShip3_redL.png", "/res/playerShip3_redR.png", "/res/playerLife3_red.png");
 	
-	private Ship(String urlShip) {
-		this.urlShip = urlShip;
+	private String urlUpShip;
+	private String urlDownShip;
+	private String urlLeftShip;
+	private String urlRightShip;
+	private String urlLife;
+	
+	private Ship(String urlUpShip, String urlDownShip, String urlLeftShip, String urlRightShip, String urlLife) {
+		this.urlUpShip = urlUpShip;
+		this.urlDownShip = urlDownShip;
+		this.urlLeftShip = urlLeftShip;
+		this.urlRightShip = urlRightShip;
+		this.urlLife = urlLife;
 	}
 	
-	public String getUrl() {
-		return this.urlShip;
+	public String getUpUrl() {
+		return this.urlUpShip;
+	}
+	
+	public String getDownUrl() {
+		return urlDownShip;
+	}
+	
+	public String getLeftUrl() {
+		return urlLeftShip;
+	}
+	
+	public String getRightUrl() {
+		return urlRightShip;
+	}
+	
+	public String getLifeUrl() {
+		return urlLife;
 	}
 
 }
