0273c9bb0bdc60337cf54a930fe531bc1558243b
Xiaoliang Pu <xxp844@cs.bham.ac.uk>
2021-05-01 20:00:30 +0000

    Update src/res/laserBlue01.png, src/res/laserRed01.png, src/view/resources/sounds/shooting-sound.mp3, src/view/BulletModel.java files

---
+++ src/view/BulletModel.java
@@ -0,0 +1,30 @@
+package view;
+
+import javafx.geometry.Point2D;
+/**
+ * The BulletModel class defines a bullet and the move action of the bullet
+ *
+ */
+public class BulletModel extends ShipModel {
+
+	public BulletModel(CellValue[][] grid) {
+		super(grid);
+	}
+
+	public boolean flyBullet() {
+		Point2D predictedShipVelocity = changeVelocity(this.currentDirection);
+
+		Point2D predictedShipLocation = shipLocation.add(predictedShipVelocity);
+		predictedShipLocation = setOffScreenLocation(predictedShipLocation);
+		// disappear when hit the wall
+		if (gameGrid[(int) predictedShipLocation.getX()][(int) predictedShipLocation.getY()] == CellValue.BLOCK) {
+			shipVelocity = changeVelocity(Direction.NONE);
+			return false;
+		} else {
+			shipVelocity = predictedShipVelocity;
+			shipLocation = predictedShipLocation;
+			// setLastDirection(this.currentDirection);
+		}
+		return true;
+	}
+}
